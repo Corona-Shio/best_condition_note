c9293ab5fa5e19e98d1b839eefcf5b77bb2237d8
Yu Nakashio <setmail_corona@icloud.com>
2024-11-27 22:23:21 +0900

    ヘッダーやフッター

--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,12 @@ Rails.application.routes.draw do
   get    "/login",   to: "sessions#new"
   post   "/login",   to: "sessions#create"
   delete "/logout",  to: "sessions#destroy"
-  # get "/users/:id/new",   to: "daily_records#new"
-  # get "/users/:id/graph", to: "daily_records#show"
+
   resources :users
   resources :daily_records do
     collection do
       get 'graph'  # /daily_records/graph にマッピング
     end
   end
-  # resources :daily_records, path: 'condition'
-  # resources :users do
-  #   member do
-  #     resources :daily_records
-  #   end
-  # end 
+  
 end
