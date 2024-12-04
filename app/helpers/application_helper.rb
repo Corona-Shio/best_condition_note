2c858b2fe1b3028384501c20f23e18f738b87095
Yu Nakashio <setmail_corona@icloud.com>
2024-12-04 10:27:58 +0900

    追加：メディアサイズによってテキストを切り替える

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,6 +10,16 @@ module ApplicationHelper
     end
   end
 
+  # ヘッダーのテキストをサイズによって切り替える
+  def responsive_text(full_text, short_text = nil)
+    short_text ||= full_text # short_textが指定されていない場合、full_textをそのまま使用
+    
+    content_tag(:span) do
+      concat(content_tag(:span, full_text,  class: 'full-text'))
+      concat(content_tag(:span, short_text, class: 'short-text'))
+    end
+  end
+
   def turbo_stream_flash
     turbo_stream.update "flash", partial: "shared/flash"
   end
