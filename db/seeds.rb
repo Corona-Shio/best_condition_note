d955671b4b2f5534bf3332f2e6253d1affc4971b
Yu Nakashio <setmail_corona@icloud.com>
2025-01-11 17:04:10 +0900

    seedsなどを元の状態に戻した

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
 
 # メインのサンプルユーザーを1人作成する
 User.create!(name:  "Example User",
@@ -16,27 +10,27 @@ User.create!(name:  "Example User",
              activated: true,
              activated_at: Time.zone.now)
 
-# # サブのサンプルユーザーを1人作成する
-# User.create!(name:  "Sample User",
-#              email: "sample@railstutorial.org",
-#              password:              "foobar",
-#              password_confirmation: "foobar",
-#              admin: false,
-#              activated: true,
-#              activated_at: Time.zone.now)
+# サブのサンプルユーザーを1人作成する
+User.create!(name:  "Sample User",
+             email: "sample@example.com",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: false,
+             activated: true,
+             activated_at: Time.zone.now)
 
-# # 追加のユーザーをまとめて生成する
-# 99.times do |n|
-#   name  = Faker::Name.name
-#   email = "example-#{n+1}@railstutorial.org"
-#   password = "password"
-#   User.create!(name:  name,
-#                email: email,
-#                password:              password,
-#                password_confirmation: password,
-#                activated: true,
-#                activated_at: Time.zone.now)
-# end
+# 追加のユーザーをまとめて生成する
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
+end
 
 # ユーザー情報の作成
 user = User.find_or_create_by(id: 1)
@@ -54,18 +48,18 @@ user = User.find_or_create_by(id: 1)
   )
 end
 
-# # ユーザー情報の作成
-# user = User.find_or_create_by(id: 2)
+# ユーザー情報の作成
+user = User.find_or_create_by(id: 2)
 
-# # 1ヶ月分のスコアデータを生成
-# (1..365).each do |day|
-#   DailyRecord.create!(
-#     user: user,
-#     date: Date.today - day,
-#     sleep:     rand(1..5),
-#     meal:      rand(1..5),
-#     mental:    rand(1..5),
-#     training:  rand(1..5),
-#     condition: rand(1..5)
-#   )
-# end
+# 1ヶ月分のスコアデータを生成
+(1..365).each do |day|
+  DailyRecord.create!(
+    user: user,
+    date: Date.today - day,
+    sleep:     rand(1..5),
+    meal:      rand(1..5),
+    mental:    rand(1..5),
+    training:  rand(1..5),
+    condition: rand(1..5)
+  )
+end
