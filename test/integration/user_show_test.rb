23b0e0a27e88b852fe976d60c74808750e7342c0
Yu Nakashio <setmail_corona@icloud.com>
2025-01-13 10:44:26 +0900

    他のユーザーを見れないように変更

--- test/integration/user_show_test.rb
+++ test/integration/user_show_test.rb
@@ -13,9 +13,17 @@ class UsersShowTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
   end
 
-  test "should display user when activated" do
+  test "should redirect when not logged in" do
+    get user_path(@activated_user)
+    assert_response :redirect
+    assert_redirected_to root_url
+  end
+
+  test "should display user when activated and logged in" do
+    log_in_as(@activated_user)
     get user_path(@activated_user)
     assert_response :success
     assert_template 'users/show'
   end
+
 end
